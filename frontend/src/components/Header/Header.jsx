92cf0a999f5b1647684b4d31d0641b8d8ac62913
Aditya <as0742004@gmail.com>
2025-01-23 22:04:21 +0530

    Header components is completed

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Order your favourite food here</h2>
+                <p>Order Food Online from Veer Ji Malai Chaap Wale Greater Noida Noida 1. Enjoy Delicious Food Delivered To Your Doorstep. Quick And Easy Online Food Delivery ....</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
